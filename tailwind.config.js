1ddc9c39050db849593fa2e62a6ce8829329d0c9
mrawildhan <mrawildhan@gmail.com>
2022-06-18 10:27:26 +0800

    Update Dark Mode + Perbaikan Fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container:{
       center: true,
@@ -11,6 +12,9 @@ module.exports = {
         primary: '#14b8a6',
         secondary: '#64748b',
         dark: '#0f172a',
+      },
+      screens: {
+        '2xl' : '1320px',
       },
     },
   },
